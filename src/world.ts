67eb7e3d7e9ed6b775f9d4f9fc3b14392d3aa683
ecotiya <ghqyydf829@gmail.com>
2021-07-14 08:11:49 +0900

    add eslint & prettier

--- src/world.ts
+++ src/world.ts
@@ -1,7 +1,13 @@
 export default class World {
-  message: string;
+  message: string
 
   constructor(message: string) {
-    this.message = message;
+    this.message = message
+  }
+
+  public sayHello(elem: HTMLElement | null): void {
+    if (elem) {
+      elem.innerText = this.message
+    }
   }
 }
