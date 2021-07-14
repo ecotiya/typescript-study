dfbecfe8f7475093018f8accd5b18ed78320d11f
ecotiya <ghqyydf829@gmail.com>
2021-07-15 08:42:36 +0900

    close

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import World from './world'
+// import World from './world'
+//
+// const root = document.getElementById('root')
+// const world = new World('Hello World!! こんにちは！')
+// world.sayHello(root)
 
-const root = document.getElementById('root')
-const world = new World('Hello World!! こんにちは！')
-world.sayHello(root)
+// 基本の型定義
+import { primitiveSample } from './basic'
+
+primitiveSample()
